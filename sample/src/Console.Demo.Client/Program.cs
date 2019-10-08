95f411d9df8e208afa1a934ebbd55e4bd0bd7401
Carlos Lisandra <carlos.lisandra@logitravel.com>
2019-10-08 11:49:13 +0200

    demo api and clients

---
+++ sample/src/Console.Demo.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo.App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            
+
+            Console.WriteLine("Greeting:" + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
